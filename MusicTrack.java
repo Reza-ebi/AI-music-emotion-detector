e067bc3af82d56dbdf1ac14c31e2cc0cbf238dd5
Reza Ebrahimi <Rezaebrahimi.edu@gmail.com>
2025-06-28 14:35:13 +0330

    MusicTrack.java

---
+++ MusicTrack.java
@@ -0,0 +1,13 @@
+public class MusicTrack {
+    public int tempo;
+    public double energy;
+    public double danceability;
+    public double valence;
+
+    public MusicTrack(int tempo, double energy, double danceability, double valence) {
+        this.tempo = tempo;
+        this.energy = energy;
+        this.danceability = danceability;
+        this.valence = valence;
+    }
+}
